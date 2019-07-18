db780f814f8a5919fac79579e52382301fa18280
brivalerio <brivalerio@gmail.com>
2019-07-18 00:26:25 -0700

    Saving app.py progress

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ import mission_to_mars_scrape.py
 
 app = Flask(__name__)
 
+# Mongo connection
+mongo = PyMongo(app,uri="mongod:localhost:27017/mars")
+
 @appp.route("/")
 def index():
+    mars_data = mongo.db.collection.find_one()
+    return render_template("index.html", data = mars_data)
 
 @app.route("/scrape")
 def scrape():
     mars_data = mission_to_mars_scrape.scrape_all()
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run()
